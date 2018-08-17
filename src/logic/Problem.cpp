2a4c840e6afb15aa3ee2074f08bacdd25295ceea
Bernhard Gleiss <bgleiss@forsyte.tuwien.ac.at>
2018-08-17 16:28:50 +0200

    added Problem class

---
+++ src/logic/Problem.cpp
@@ -0,0 +1,36 @@
+#include "Problem.hpp"
+#include "Output.hpp"
+
+namespace logic {
+    
+    void Problem::outputSMTLIB(std::ostream& ostr)
+    {        
+        // output sort declarations
+        for(const auto& pair : Sorts::nameToSort())
+        {
+            ostr << declareSortSMTLIB(*pair.second);
+        }
+        
+        // output symbol definitions
+        for (const auto& symbol : Signature::signature())
+        {
+            ostr << symbol->declareSymbolSMTLIB();
+        }
+        
+        // output each axiom
+        for (const auto& axiom : axioms)
+        {
+            ostr << axiom.second->declareSMTLIB(axiom.first) << std::endl;
+        }
+
+        // output each lemma
+        for (const auto& lemma : lemmas)
+        {
+            ostr << lemma.second->declareSMTLIB(lemma.first) << std::endl;
+        }
+        
+        // output conjecture
+        ostr << conjecture->declareSMTLIB("conjecture", true) << std::endl;
+    }
+
+}
---
+++ src/logic/Problem.hpp
@@ -0,0 +1,24 @@
+#ifndef __Problem__
+#define __Problem__
+
+#include <utility>
+#include <string>
+#include "Formula.hpp"
+
+namespace logic {
+    
+    // represents a first-order problem which can be passed to a prover.
+    // for now sorts and signatures are not part of Problem, maybe should be refactored.
+    class Problem
+    {
+        Problem() : axioms(), conjecture(nullptr), lemmas() {}
+        
+        std::vector<std::pair<std::string, std::shared_ptr<const Formula>>> axioms;
+        std::shared_ptr<const Formula> conjecture;
+        
+        std::vector<std::pair<std::string, std::shared_ptr<const Formula>>> lemmas;
+        
+        void outputSMTLIB(std::ostream& ostr);
+    };
+}
+#endif

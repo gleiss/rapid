affaa606d4860d231c5181f8e862d6cd52337a7d
gleiss <bg890@gmx.at>
2019-01-03 14:46:42 +0100

    refactor parser code from main() into WhileParserWrapper

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,18 @@
 #include "logic/Theory.hpp"
 #include "logic/Problem.hpp"
 
-#include "parser/WhileParsingContext.hpp"
+#include "program/Program.hpp"
+
 #include "util/Options.hpp"
 #include "util/Output.hpp"
 
-#include "parser/WhileScanner.cpp"
-#include "parser/WhileParser.hpp"
+#include "parser/WhileParserWrapper.hpp"
 
 #include "analysis/Semantics.hpp"
 #include "analysis/Analyzer.hpp"
 #include "analysis/Properties.hpp"
 #include "analysis/PropertiesTime.hpp"
 
-#include "program/Program.hpp"
 
 extern FILE *yyin;
 
@@ -40,56 +39,18 @@ int main(int argc, char *argv[])
             {
                 std::string inputFile = argv[argc - 1];
 
-                // test readbility, easier than catching exception thrown by parser
-                std::ifstream istr(inputFile);
-                if (!istr)
-                {
-                    std::cerr << "Unable to read file " << inputFile << std::endl;
-                    return 0;
-                }
-                
-                yy_flex_debug = false;
-                
-                // set parser input to inputFile
-                FILE* f = fopen(inputFile.c_str(), "r");
-                yy_buffer_state*bp = yy_create_buffer(f, YY_BUF_SIZE );
-                yy_create_buffer(f,YY_BUF_SIZE);
-                yy_switch_to_buffer(bp);
-                
-                // generate a context, whose fields are used as in/out-parameters for parsing
-                parser::WhileParsingContext context;
-                context.inputFile = inputFile;
-                
-                // parse the input-program into context
-                parser::WhileParser parser(context);
-                parser.set_debug_level(false);
-                parser.parse();
-                fclose(f);
-                
-                if (context.errorFlag)
-                {
-                    exit(1);
-                }
-
-                assert(context.program);
-                assert(context.programGlobalProperties);
-                assert(context.conjecture);
-
-                util::Output::stream() << util::Output::comment;
-                util::Output::stream() << *context.program;
-                util::Output::stream() << util::Output::nocomment;
-
-                // end of parsing
+                auto parserResult = parser::parse(inputFile);
                 
                 logic::Problem problem;
                 
-                analysis::Semantics s(*context.program, *context.programGlobalProperties, context.locationToActiveVars);
+                analysis::Semantics s(*parserResult.program, *parserResult.programGlobalProperties, parserResult.locationToActiveVars);
                 
                 problem.axioms = s.generateSemantics();
-                problem.conjecture = std::move(context.conjecture);
+                problem.conjecture = std::move(parserResult.conjecture);
                 
-                analysis::TraceLemmas generator(*context.program, *context.programGlobalProperties, context.locationToActiveVars);
-                problem.lemmas = generator.generate();
+                analysis::TraceLemmas traceLemmas(*parserResult.program, *parserResult.programGlobalProperties, parserResult.locationToActiveVars);
+                problem.lemmas = traceLemmas.generate();
+                
                 problem.outputSMTLIB(util::Output::stream());
             }
         }
